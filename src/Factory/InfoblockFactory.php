db84d126d307e59c21206a2e3f100065b32accc6
Juggerscript <shredder139@gmail.com>
2021-08-18 09:22:47 +0700

    init commit

---
+++ src/Factory/InfoblockFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Factory;
+use Letsrock\CreatableModel\CreatableModel;
+use Letsrock\Factory\ModelFactoryInterface;
+use Faker\Factory as FakerFactory;
+
+abstract class InfoblockFactory implements ModelFactoryInterface
+{
+    public $faker;
+
+    public function __construct()
+    {
+        $this->faker = FakerFactory::create();
+    }
+
+    public function make(int $quantity): bool
+    {
+        return false;
+    }
+
+    public function makeByModel(CreatableModel $model, int $quantity): bool
+    {
+        if (!$quantity) return false;
+
+        $counter = 0;
+        while ($counter < $quantity) {
+            $data = $this->definition();
+
+            $model->createByFactory($data);
+            $counter++;
+        }
+
+        return true;
+    }
+
+    /** Gets data by $this->faker */
+    abstract function definition(): array;
+}
